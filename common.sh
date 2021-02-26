7d5c5f1dce9f32890a4ab6e4601f49d586e11326
Kid <kidonng@gmail.com>
2021-02-27 03:14:45 +0800

    Configure

---
+++ common.sh
@@ -0,0 +1,4 @@
+brew install bat exa fd fish fzf ripgrep starship
+
+fish -c "curl -sSL https://git.io/fish-plug | source && plug install kidonng/fish-plug"
+fish -c "plug install nickeb96/puffer-fish PatrickF1/fzf.fish kidonng/fish-starship jorgebucaran/autopair.fish"
